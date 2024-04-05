94b1809327dff0926929a5d2597ee564ae94f0e5
rian <rian@physics.ox.ac.uk>
2024-04-05 12:41:40 +0300

    outline test

--- starknetOSInput/core/starknet_os_input_test.go
+++ starknetOSInput/core/starknet_os_input_test.go
@@ -8,12 +8,14 @@ import (
 	"go.uber.org/mock/gomock"
 )
 
+// Note: given the depreceated classes (dummy_token,dummy_account,token_for_testing)
+// and initial transactions (deploy_token_tx, fund_account_tx, deploy_account_tx)
+// we should be able to build the same initial and new state as run_os.py
+// Given these two states+txns etc, we should be able to test GenerateStarknetOSInput (==get_os_hints).
+// The final goal is to compute a StarknetOsInput equivalent to testdata/os_input.json
 func TestGenerateStarknetOSInput(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	t.Cleanup(mockCtrl.Finish)
-	// stateReader := mocks.NewMockStateHistoryReader(mockCtrl)
-	// block := &core.Block{}
-	// vmInput := VMParameters{}
 
 	t.Run("empty test", func(t *testing.T) {
 		result, err := TxnExecInfo(nil)
